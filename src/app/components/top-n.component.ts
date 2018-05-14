728a52177bd24853186dfa6d5d63273cc5dcc691
Lukas Rath <l.rath@netcononmy.net>
2018-05-14 12:24:27 +0200

    added emoji code in sort calculation

--- src/app/components/top-n.component.ts
+++ src/app/components/top-n.component.ts
@@ -35,27 +35,56 @@ export class TopNComponent implements OnInit, OnDestroy {
       .emojiUpdatesNotify()
       .subscribe((data: EmojiData) => {
         console.log(`\n---------------------------------------`);
-        console.log(`[Top-N] Update by emoji (${data.emoji}) Object-Count: ${this.emojiDataObjects.length}`);
+        console.log(
+          `[Top-N] Update by emoji (${data.emoji}) Object-Count: ${
+            this.emojiDataObjects.length
+          }`
+        );
         const i = this.emojiDataObjects.findIndex(
           (object: EmojiData) => object.emoji === data.emoji
         );
         console.log(`[Top-N] Object-Index: ${i}`);
         if (i > -1) {
           if (this.emojiDataObjects[i].count !== data.count) {
-            console.log(`[Top-N] Will Update, Old count: ${this.emojiDataObjects[i].count} / New count: ${data.count}`);
-            this.emojiDataObjects[i] = {...data, updated: true};
-            console.log(`[Top-N] Updated index (${i}), count: ${this.emojiDataObjects[i].count}, emoji: ${this.emojiDataObjects[i].emoji}`);
+            console.log(
+              `[Top-N] Will Update, Old count: ${
+                this.emojiDataObjects[i].count
+              } / New count: ${data.count}`
+            );
+            this.emojiDataObjects[i] = { ...data, updated: true };
+            console.log(
+              `[Top-N] Updated index (${i}), count: ${
+                this.emojiDataObjects[i].count
+              }, emoji: ${this.emojiDataObjects[i].emoji}`
+            );
             this.ref.detectChanges();
             this.emojiDataObjects = this.emojiDataObjects.sort(
-              (a: EmojiData, b: EmojiData) => b.count - a.count
+              (a: EmojiData, b: EmojiData) => {
+                return (
+                  (b.count + 1 / b.emoji.charCodeAt(1)) -
+                  (a.count + 1 / a.emoji.charCodeAt(1))
+                );
+              }
             );
-            console.log(`[Top-N] Object-Count after sorting: ${this.emojiDataObjects.length}`);
-            console.log(`[Top-N] Begin animation of ${this.emojiDataObjects[i].emoji}`);
+            console.log(
+              `[Top-N] Object-Count after sorting: ${
+                this.emojiDataObjects.length
+              }`
+            );
+            console.log(
+              `[Top-N] Begin animation of ${this.emojiDataObjects[i].emoji}`
+            );
             setTimeout(() => {
-              console.log(`[Top-N] End animation of ${this.emojiDataObjects[i].emoji}`);
+              console.log(
+                `[Top-N] End animation of ${this.emojiDataObjects[i].emoji}`
+              );
               this.emojiDataObjects[i].updated = false;
               this.ref.detectChanges();
-              console.log(`[Top-N] Object-Count after animation: ${this.emojiDataObjects.length}`);
+              console.log(
+                `[Top-N] Object-Count after animation: ${
+                  this.emojiDataObjects.length
+                }`
+              );
               console.log(`---------------------------------------\n`);
             }, 500);
           }
