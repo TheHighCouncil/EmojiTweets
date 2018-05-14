e573cc81104ac025dade6e58c4c64de22dfed8eb
Lukas Rath <l.rath@netcononmy.net>
2018-05-14 11:21:37 +0200

    added logging for Top-N

--- src/app/components/top-n.component.ts
+++ src/app/components/top-n.component.ts
@@ -34,21 +34,33 @@ export class TopNComponent implements OnInit, OnDestroy {
     this.emojiUpdatesNotifySubject = this.emojiTrackerService
       .emojiUpdatesNotify()
       .subscribe((data: EmojiData) => {
+        console.log(`\n---------------------------------------`);
+        console.log(`[Top-N] Update by emoji (${data.emoji}) Object-Count: ${this.emojiDataObjects.length}`);
         const i = this.emojiDataObjects.findIndex(
           (object: EmojiData) => object.emoji === data.emoji
         );
+        console.log(`[Top-N] Object-Index: ${i}`);
         if (i > -1) {
           if (this.emojiDataObjects[i].count !== data.count) {
-            this.emojiDataObjects[i].updated = true;
+            console.log(`[Top-N] Will Update, Old count: ${this.emojiDataObjects[i].count} / New count: ${data.count}`);
+            this.emojiDataObjects[i] = {...data, updated: true};
+            console.log(`[Top-N] Updated index (${i}), count: ${this.emojiDataObjects[i].count}, emoji: ${this.emojiDataObjects[i].emoji}`);
             this.ref.detectChanges();
+            this.emojiDataObjects = this.emojiDataObjects.sort(
+              (a: EmojiData, b: EmojiData) => b.count - a.count
+            );
+            console.log(`[Top-N] Object-Count after sorting: ${this.emojiDataObjects.length}`);
+            console.log(`[Top-N] Begin animation of ${this.emojiDataObjects[i].emoji}`);
             setTimeout(() => {
-              this.emojiDataObjects[i] = {...data, updated: false};
-              this.emojiDataObjects = this.emojiDataObjects.sort(
-                (a: EmojiData, b: EmojiData) => b.count - a.count
-              );
+              console.log(`[Top-N] End animation of ${this.emojiDataObjects[i].emoji}`);
+              this.emojiDataObjects[i].updated = false;
               this.ref.detectChanges();
+              console.log(`[Top-N] Object-Count after animation: ${this.emojiDataObjects.length}`);
+              console.log(`---------------------------------------\n`);
             }, 500);
           }
+        } else {
+          console.log(`---------------------------------------\n`);
         }
       });
   }
